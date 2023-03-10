2c7dead64bc0bae60c0cb4c21fdc6a3cc9f692ba
mayito9612 <maario.9612@gmail.com>
2023-03-10 17:19:03 -0600

    Validacion de telefono

--- src/main/java/com/bedu/java/postwork4/postwork4/Postwork4Application.java
+++ src/main/java/com/bedu/java/postwork4/postwork4/Postwork4Application.java
@@ -1,6 +1,9 @@
 package com.bedu.java.postwork4.postwork4;
 
 import com.bedu.java.postwork4.postwork4.model.Persona;
+import com.bedu.java.postwork4.postwork4.service.FormateadorTelefono;
+import com.bedu.java.postwork4.postwork4.service.ValidadorTelefono;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -15,19 +18,47 @@ public class Postwork4Application implements CommandLineRunner {
 		SpringApplication.run(Postwork4Application.class, args);
 	}
 
-	@Override
-	public void run(String... args) throws Exception{
-		Scanner leer = new Scanner(System.in);
+	private final ValidadorTelefono validadorTelefono;
+	private final FormateadorTelefono formateadorTelefono;
 
-		System.out.println("Introduce tu nombre: ");
-		String nombre = leer.nextLine();
+
+	@Autowired
+	public Postwork4Application(ValidadorTelefono validadorTelefono, FormateadorTelefono formateadorTelefono) {
+		this.validadorTelefono = validadorTelefono;
+		this.formateadorTelefono = formateadorTelefono;
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		Scanner reader = new Scanner(System.in);
+
+
+		System.out.println("Introduce el nombre: ");
+		String nombre = reader.nextLine();
+
 
 		System.out.println("Introduce el telefono: ");
-		String telefono = leer.nextLine();
+		String telefono = reader.nextLine();
 
-		Persona persona = new Persona(nombre, telefono);
 
-		System.out.println(persona);
+		if (validadorTelefono.isValido(telefono)) {
+			telefono = validadorTelefono.limpiaNumero(telefono);
+			telefono = formateadorTelefono.formatea(telefono);
+
+
+			Persona persona = new Persona(nombre, telefono);
+
+
+			System.out.println(persona);
+		} else {
+			System.out.println("Por favor, introduce un numero valido");
+		}
+
 	}
+
+
+
+
+
 
 }
---
+++ src/main/java/com/bedu/java/postwork4/postwork4/service/ValidadorTelefono.java
@@ -0,0 +1,17 @@
+package com.bedu.java.postwork4.postwork4.service;
+
+import org.springframework.stereotype.Service;
+
+import java.util.regex.Pattern;
+
+@Service
+public class ValidadorTelefono {
+
+    private static final Pattern PATTERN_TELEFONO = Pattern.compile("^(\\d{2,4}[- .]?){2}\\d{4}$");
+    public boolean isValido(String telefono){
+        return PATTERN_TELEFONO.matcher(telefono).matches();
+    }
+    public String limpiaNumero(String telefono){
+        return telefono.replaceAll("[^0-9]", "");
+    }
+}
